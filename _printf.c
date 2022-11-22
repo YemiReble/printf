a54ec9641e6612770ba35ccd315cbcda427a7db7
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2022-11-22 16:37:04 +0000

    My Printf Function

--- _printf.c
+++ _printf.c
@@ -1,61 +1,47 @@
 #include "main.h"
-#include <stdarg.h>
-
-
 /**
-  * _printf - creating my own printf function
-  * @format: type of argument passed to fuction
-  *
-  * Return: the character printed
-  */
+ * _printf - function to print anything
+ * @format: types of argument passed to the function
+ *
+ *  Return: number of characters printed
+ */
 
 int _printf(const char *format, ...)
 {
-	int i = 0;
-	int count = 0;
-	int value = 0;
+	int check = 0, i;
 	va_list args;
-	int (*spe)(va_list);
+	int (*func)(va_list);
 
 	va_start(args, format);
 
 	if (format == NULL)
 		return (-1);
 
-	while (format[i])
+	for (i = 0; format[i]; i++)
 	{
-		if (format[i] != '%')
-		{
-			value = write(1, &format[i], 1);
-			count = count + value;
-			i++;
-			continue;
-		}
-
 		if (format[i] == '%')
 		{
-			spe = check_specifier(&format[i + 1]);
-			if (spe != NULL)
-			{
-				value = spe(args);
-				count = count + value;
-				i = i + 2;
-				continue;
-			}
-			if (format[i + 1] == '\0')
-			{
-				break;
-			}
-			if (format[i + 1] != '\0')
-			{
-				value = write(1, &format[i + 1], 1);
-				count = count + value;
-				i = i + 2;
-				continue;
-			}
+			i++;
+			if (!(format[i]))
+				return (-1);
 
+			func = specifier(format[i]);
+
+			if (func == NULL)
+			{
+				_write('%');
+				_write(format[i]);
+				check += 2;
+			}
+			else
+				check += func(args);
+		}
+		else
+		{
+			_write(format[i]);
+			check++;
 		}
 	}
 	va_end(args);
-	return (count);
+	return (check);
 }
