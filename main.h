2670acfe929ae1e9c60d5acb2b0ff762dd3bb299
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2022-11-22 15:26:07 +0000

    My Prinf Header File

--- main.h
+++ main.h
@@ -1,8 +1,30 @@
 #ifndef MAIN_H
-#define MAIN_H
+#define MAIN_N
 
 #include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
 
-int _printf(const char *format, ...);
+int _printf(const char *formart, ...);
+int (*check_specifier(const char*))(va_list);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_mod(va_list args);
+int print_integer(va_list args);
+int print_dec(va_list args);
 
-#endif /*MAIN_H*/
+
+
+/**
+  * struct func - structure for specifier to printer
+  * @t: character to compare
+  * @f: function to handle printing
+  */
+typedef struct func
+{
+	char *t;
+	int (*f)(va_list);
+} func_t;
+
+#endif
